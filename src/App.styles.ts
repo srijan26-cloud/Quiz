f7c91a960082ff8bb7e991a9bbac0146f74c7dd0
Srijan Chandra <81234545+srijan26-cloud@users.noreply.github.com>
2021-05-15 00:30:12 +0530

    Updated

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,13 +1,11 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import BGIMG from './images/img.jpg';
 
 export const GlobalStyle = createGlobalStyle`
     html {
         height : 100%;
     }
     body {
-        background-image : url(${BGIMG});
-        background-size : cover;
+        background-color: #eee;
         margin : 0;
         padding : 0 20px;
         display : flex;
@@ -29,30 +27,36 @@ export const Wrapper = styled.div`
             color : #fff;
         }
     .score {
-        color : #fff;
+        color : #aaa;
         font-size : 2rem;
         margin : 0;
     }
     h1{
         font-family : Algerian;
-        font-size : 70px;
+        font-size : 55px;
         text-align : center;
         margin : 20px;
         font-weight:400;
-        color:white;
+        color:orange;
         background : transparent; 
     }
-    
+    h3{
+        font-style : italic;
+        color:orange;
+    }
     .start , .next {
         cursor : pointer;
-        border : 2px solid #d38558;
-        box-shadow : 0px 5px 10px rgba(0,0,0,0.25);
+        background-color: white;
+        color: black;
         border-radius : 10px;
         height : 40px;
         margin : 20px 0;
         padding : 0 40px;
     }
-
+    .start:hover , .next:hover {
+        cursor : pointer;
+        background-color: green;
+        color: #fff;
     .start{
        max-width:200px; 
     }
